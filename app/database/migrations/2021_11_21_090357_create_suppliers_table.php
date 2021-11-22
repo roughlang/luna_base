aaaa1849d96df8e4b1a533be85eb37ac7f88571d
osamu yamakami <osamuyamakami@cpe-172-101-20-99.maine.res.rr.com>
2021-11-22 10:01:23 +0900

    supplier adminのマイグレーションファイルのテーブル定義

--- app/database/migrations/2021_11_21_090357_create_suppliers_table.php
+++ app/database/migrations/2021_11_21_090357_create_suppliers_table.php
@@ -14,8 +14,13 @@ class CreateSuppliersTable extends Migration
     public function up()
     {
         Schema::create('suppliers', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+          $table->id();
+          $table->string('name');
+          $table->string('email')->unique();
+          $table->timestamp('email_verified_at')->nullable();
+          $table->string('password');
+          $table->rememberToken();
+          $table->timestamps();
         });
     }
 
--- app/database/migrations/2021_11_21_090403_create_admins_table.php
+++ app/database/migrations/2021_11_21_090403_create_admins_table.php
@@ -14,8 +14,13 @@ class CreateAdminsTable extends Migration
     public function up()
     {
         Schema::create('admins', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+          $table->id();
+          $table->string('name');
+          $table->string('email')->unique();
+          $table->timestamp('email_verified_at')->nullable();
+          $table->string('password');
+          $table->rememberToken();
+          $table->timestamps();
         });
     }
 
